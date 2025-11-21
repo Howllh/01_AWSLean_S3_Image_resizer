62fa6100396c780cb40e692a7bf51002cc37f128
YAN <genko.yanhao@gmail.com>
2025-11-21 15:13:15 +0900

    Lambda Code

---
+++ 01_s3_image_resizer/lambda_function.py
@@ -0,0 +1,31 @@
+import boto3
+import os
+import sys
+import uuid
+from urllib.parse import unquote_plus
+from PIL import Image
+import PIL.Image
+
+s3_client = boto3.client('s3')
+TARGET_BUCKET = 'hao-image-resize-output' 
+
+def resize_image(image_path, resized_path):
+    with Image.open(image_path) as image:
+        image.thumbnail((1280, 1280))
+        image.save(resized_path)
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = unquote_plus(record['s3']['object']['key'])
+        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
+        upload_path = '/tmp/resized-{}'.format(key)
+        
+        try:
+            s3_client.download_file(bucket, key, download_path)
+            resize_image(download_path, upload_path)
+            s3_client.upload_file(upload_path, TARGET_BUCKET, key)
+            print(f"Success! Resized {key}")
+        except Exception as e:
+            print(f"Error: {e}")
+            raise e
